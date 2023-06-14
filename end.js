6e7f21a6f658a885dd615e0a01e64acf7b5f6847
ishikadevare13 <ishikadevare13@gmail.com>
2023-06-14 15:44:53 +0530

    New Files added

---
+++ end.js
@@ -0,0 +1,17 @@
+const username = document.getElementById("username");
+const saveScoreBtn = document.getElementById("saveScoreBtn");
+const finalScore = document.getElementById("finalScore");
+const mostRecentScore = localStorage.getItem("mostRecentScore");
+finalScore.innerText = mostRecentScore;
+
+username.addEventListener('keyup', () => {
+    saveScoreBtn.disabled = !username.value;
+});
+
+
+
+
+saveHighScore = (e) => {
+    console.log("Clicked the save btn");
+    e.preventDefault();
+};
