b24b984030d49d0da9142b04b297edda655d78fe
ishikadevare13 <ishikadevare13@gmail.com>
2023-06-15 12:59:32 +0530

    Finally completed

--- game.js
+++ game.js
@@ -3,7 +3,8 @@ const choices = Array.from(document.getElementsByClassName("choice-text"));
 const progressText = document.getElementById('progressText');
 const scoreText = document.getElementById('score');
 const progressBarFull = document.getElementById('progressBarFull');
-
+const loader = document.getElementById('loader');
+const game = document.getElementById('game');
 
 let currentQuestion = {};
 let acceptingAnswers = true;
@@ -35,6 +36,7 @@ fetch("https://opentdb.com/api.php?amount=10&category=9&difficulty=easy&type=mul
 
             return formattedQuestion;
         });
+        
         startGame();
     })
     .catch(err => {
@@ -50,6 +52,8 @@ startGame = () => {
     score = 0;
     availableQuestions = [ ...questions];
     getNewQuestion();
+    game.classList.remove("hidden");
+    loader.classList.add("hidden");
 };
 
 getNewQuestion = () => {
