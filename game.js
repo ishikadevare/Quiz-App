c558356e34d0cbe7d98301b7595607262e88a8bf
ishikadevare13 <ishikadevare13@gmail.com>
2023-06-15 12:39:22 +0530

    Added trivia api

--- game.js
+++ game.js
@@ -13,13 +13,28 @@ let availableQuestions = [];
 
 let questions = [];
 
-fetch("questions.json")
+fetch("https://opentdb.com/api.php?amount=10&category=9&difficulty=easy&type=multiple")
     .then(res => {
         return res.json();
     })
     .then(loadedQuestions => {
-        console.log(loadedQuestions);
-        questions = loadedQuestions;
+        console.log(loadedQuestions.results);
+        questions = loadedQuestions.results.map(loadedQuestions => {
+            const formattedQuestion = {
+                question: loadedQuestions.question
+            };
+
+            const answerChoices = [ ... loadedQuestions.incorrect_answers];
+            formattedQuestion.answer = Math.floor(Math.random()*3) + 1;
+            answerChoices.splice(formattedQuestion.answer -1, 0, 
+            loadedQuestions.correct_answer);
+
+            answerChoices.forEach((choice, index) => {
+                formattedQuestion["choice" + (index+1)] = choice;
+            })
+
+            return formattedQuestion;
+        });
         startGame();
     })
     .catch(err => {
